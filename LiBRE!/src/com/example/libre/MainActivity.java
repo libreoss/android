26f5e57ec6178c171a1491bf66545d0398230b4b
Nikola Hardi <atlantic777@lugons.org>
2013-08-25 21:43:07 +0200

    Completely removed RSS

--- LiBRE!/src/com/example/libre/MainActivity.java
+++ LiBRE!/src/com/example/libre/MainActivity.java
@@ -36,208 +36,71 @@ import android.os.AsyncTask;
 @SuppressWarnings("unused")
 public class MainActivity extends SherlockActivity {
 
-	private String feedUrl = "http://libre.lugons.org/index.php/category/librevesti/feed/";
 	private Post samplePost;
-	
-	private class BackgroundDownload extends AsyncTask<Void, Void, RSSFeed>
-	{
-		protected RSSFeed doInBackground(Void... params) 
-		{
-			return getFeed(feedUrl);
-		}
 
-		protected void onPostExecute(RSSFeed result)
-		{
-			UpdateDisplay(result);
-		}
+	private void UpdateDisplay() {
+		TextView feedtitle = (TextView) findViewById(R.id.feedtitle);
+		TextView feedpubdate = (TextView) findViewById(R.id.feedpubdate);
+		ListView itemlist = (ListView) findViewById(R.id.itemlist);
+		List<Post> postList = new Vector<Post>(0);
+
+		postList.add(samplePost);
+		postList.add(samplePost);
+
+		feedtitle.setVisibility(View.GONE);
+		feedpubdate.setText(samplePost.getPubDate().toString());
+
+		ArrayAdapter<Post> adapter = new ArrayAdapter<Post>(this,
+				android.R.layout.simple_list_item_1, postList);
+
+		itemlist.setAdapter(adapter);
+
+		itemlist.setSelection(0);
+
+		final SherlockActivity mainActivityRef = this;
+
+		itemlist.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parrent, View v, int index,
+					long l) {
+				Intent descIntent = new Intent(mainActivityRef,
+						ShowDescription.class);
+
+				Bundle b = new Bundle();
+
+				b.putString("title", samplePost.getTitle());
+				b.putString("description", samplePost.getContent());
+				b.putString("link", samplePost.getUrl());
+				b.putString("pubdate", samplePost.getPubDate().toString());
+				descIntent.putExtra("android.intent.extra.INTENT", b);
+
+				startActivity(descIntent);
+			}
+		});
+
 	}
 
-	private RSSFeed getFeed(String urlToRssFeed)
-	{
-		
-		try
-		{
-			Downloader d = new Downloader(urlToRssFeed);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-			// setup the URL
-			URL url = d.downloadToFile(getFilesDir(), "rss.xml");
+		TabHost tabs = (TabHost) findViewById(R.id.TabHost01);
+		tabs.setup();
 
-			// create the factory
-			SAXParserFactory factory = SAXParserFactory.newInstance();
-			// create a parser
-			SAXParser parser = factory.newSAXParser();
-			
-			// create the reader (scanner)
-			XMLReader xmlreader = parser.getXMLReader();
-			// instantiate our handler
-			RSSHandler theRssHandler = new RSSHandler();
-			// assign our handler
-			xmlreader.setContentHandler(theRssHandler);
+		TabHost.TabSpec spec1 = tabs.newTabSpec("tag1");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("News");
+		tabs.addTab(spec1);
 
-			// get our data through the URL class
-			InputSource is = new InputSource(url.openStream());
-			// perform the synchronous parse           
-			xmlreader.parse(is);
-			// get the results - should be a fully populated RSSFeed instance, 
-			// or null on error
-			return theRssHandler.getFeed();
-		}
-		catch (Exception ee)
-		{
-			// if you have a problem, simply return null
-			return null;
-		}
+		TabHost.TabSpec spec2 = tabs.newTabSpec("tag2");
+		spec2.setContent(R.id.tab2);
+		spec2.setIndicator("Latest");
+		tabs.addTab(spec2);
+
+		samplePost = new Post();
+
+		UpdateDisplay();
+
 	}
-	
-    private void UpdateDisplay(RSSFeed feed)
-    {
-        TextView feedtitle = (TextView) findViewById(R.id.feedtitle);
-        TextView feedpubdate = (TextView) findViewById(R.id.feedpubdate);
-        ListView itemlist = (ListView) findViewById(R.id.itemlist);
-        
-        
-        if (feed == null)
-        {
-            feedtitle.setText("No RSS Feed Available");
-            return;
-        }
-        
-        
-        feedtitle.setVisibility(View.GONE);
-        feedpubdate.setText(feed.getPubDate());
-
-        
-        ArrayAdapter<RSSItem> adapter = new
-        ArrayAdapter<RSSItem>(this,android.R.layout.
-        simple_list_item_1,feed.getAllItems());
-
-        itemlist.setAdapter(adapter);
-        
-        itemlist.setSelection(0);      
-	
-	// little crazy stuff xD 
-	final SherlockActivity mainActivityRef = this; 
-	final RSSFeed feedRef = feed;
-	
-	itemlist.setOnItemClickListener(new OnItemClickListener() 
-	{
-		@Override 
-		public void onItemClick(AdapterView<?> parrent, View v,int index, long l) 
-		{
-			Intent descIntent = new Intent(mainActivityRef, ShowDescription.class);
-			
-			Bundle b = new Bundle();
-
-			RSSItem rssitem = feedRef.getItem(index);
-			
-			b.putString("title", rssitem.getTitle());
-			b.putString("description", rssitem.getDescription());
-			b.putString("link", rssitem.getLink());
-			b.putString("pubdate", rssitem.getPubDate());
-			descIntent.putExtra("android.intent.extra.INTENT", b);
-
-			startActivity(descIntent);
-		}
-	});
-    
-        
-    }
-    
-    private void UpdateDisplay()
-    {
-        TextView feedtitle = (TextView) findViewById(R.id.feedtitle);
-        TextView feedpubdate = (TextView) findViewById(R.id.feedpubdate);
-        ListView itemlist = (ListView) findViewById(R.id.itemlist);
-        List<Post> postList = new Vector<Post>(0);
-
-        
-        feedtitle.setVisibility(View.GONE);
-        feedpubdate.setText(samplePost.getPubDate().toString());
-
-        
-        ArrayAdapter<Post> adapter = new
-        ArrayAdapter<Post>(this,android.R.layout.
-        simple_list_item_1,postList);
-
-        itemlist.setAdapter(adapter);
-        
-        itemlist.setSelection(0);      
-	
-	// little crazy stuff xD 
-	final SherlockActivity mainActivityRef = this; 
-	//final RSSFeed feedRef = feed;
-
-	
-	itemlist.setOnItemClickListener(new OnItemClickListener() 
-	{
-		@Override 
-		public void onItemClick(AdapterView<?> parrent, View v,int index, long l) 
-		{
-			Intent descIntent = new Intent(mainActivityRef, ShowDescription.class);
-			
-			Bundle b = new Bundle();
-			
-			b.putString("title", samplePost.getTitle());
-			b.putString("description", samplePost.getContent());
-			//b.putString("link", rssitem.getLink());
-			b.putString("pubdate", samplePost.getPubDate().toString());
-			descIntent.putExtra("android.intent.extra.INTENT", b);
-
-			startActivity(descIntent);
-		}
-	});
-    
-        
-    }
-    
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    
-	TabHost tabs = (TabHost)findViewById(R.id.TabHost01);
-	tabs.setup();
-
-	TabHost.TabSpec spec1 = tabs.newTabSpec("tag1");
-	spec1.setContent(R.id.tab1);
-	spec1.setIndicator("News");
-	tabs.addTab(spec1);
-
-	TabHost.TabSpec spec2 = tabs.newTabSpec("tag2");
-	spec2.setContent(R.id.tab2);
-	spec2.setIndicator("Latest");
-	tabs.addTab(spec2);
-	
-	samplePost = new Post();
-	
-	//UpdateDisplay();
-	
-	BackgroundDownload task = new BackgroundDownload();
-	task.execute();
-    }
-
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getSupportMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) 
-    {
-    	switch (item.getItemId()) 
-	{
-		case R.id.action_reload:
-			BackgroundDownload task = new BackgroundDownload();
-			task.execute();
-			return true; 
-		default: 
-			return super.onOptionsItemSelected(item);
-	}
-    }
-    
-    
 }
