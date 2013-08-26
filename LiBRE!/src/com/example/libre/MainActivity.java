6967af71148a2c828e067b66b2f1d20197eb9047
Nikola Hardi <atlantic777@lugons.org>
2013-08-26 20:19:19 +0200

    Swipe base code

--- LiBRE!/src/com/example/libre/MainActivity.java
+++ LiBRE!/src/com/example/libre/MainActivity.java
@@ -1,44 +1,26 @@
 package com.example.libre;
 
-import java.net.URL;
-import java.util.List;
-import java.util.Vector;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
-import com.actionbarsherlock.view.MenuInflater;
-
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
-//import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.TabHost;
-
-import android.os.AsyncTask;
 
 @SuppressWarnings("unused")
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends FragmentActivity {
+	ViewPager mViewPager;
+    SectionsPagerAdapter mSectionsPagerAdapter;
 
 	private Post samplePost;
-
+	/*
 	private void UpdateDisplay() {
+		
 		TextView feedtitle = (TextView) findViewById(R.id.feedtitle);
 		TextView feedpubdate = (TextView) findViewById(R.id.feedpubdate);
 		ListView itemlist = (ListView) findViewById(R.id.itemlist);
@@ -57,50 +39,73 @@ public class MainActivity extends SherlockActivity {
 
 		itemlist.setSelection(0);
 
-		final SherlockActivity mainActivityRef = this;
-
-		itemlist.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parrent, View v, int index,
-					long l) {
-				Intent descIntent = new Intent(mainActivityRef,
-						ShowDescription.class);
-
-				Bundle b = new Bundle();
-
-				b.putString("title", samplePost.getTitle());
-				b.putString("description", samplePost.getContent());
-				b.putString("link", samplePost.getUrl());
-				b.putString("pubdate", samplePost.getPubDate().toString());
-				descIntent.putExtra("android.intent.extra.INTENT", b);
-
-				startActivity(descIntent);
-			}
-		});
-
 	}
+	*/
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		TabHost tabs = (TabHost) findViewById(R.id.TabHost01);
-		tabs.setup();
+        mSectionsPagerAdapter = new SectionsPagerAdapter(
+        getSupportFragmentManager());
 
-		TabHost.TabSpec spec1 = tabs.newTabSpec("tag1");
-		spec1.setContent(R.id.tab1);
-		spec1.setIndicator("News");
-		tabs.addTab(spec1);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-		TabHost.TabSpec spec2 = tabs.newTabSpec("tag2");
-		spec2.setContent(R.id.tab2);
-		spec2.setIndicator("Latest");
-		tabs.addTab(spec2);
-
-		samplePost = new Post();
-
-		UpdateDisplay();
+		//samplePost = new Post();
+		//UpdateDisplay();
 
 	}
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = new DummySectionFragment();
+            Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+            case 0:
+                return getString(R.string.title_section1).toUpperCase();
+            case 1:
+                return getString(R.string.title_section2).toUpperCase();
+            case 2:
+                return getString(R.string.title_section3).toUpperCase();
+            }
+            return null;
+        }
+    }
+
+    public static class DummySectionFragment extends Fragment {
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public DummySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            TextView textView = new TextView(getActivity());
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(Integer.toString(getArguments().getInt(
+                    ARG_SECTION_NUMBER)));
+            return textView;
+        }
+    }
+
 }
